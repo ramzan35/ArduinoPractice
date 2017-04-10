3ae19061c1910def0a9ab3375f1fee3360a70727
MohoRamzan <ramzandieze35@gmail.com>
2017-04-11 00:50:16 +0530

    Add files via upload

---
+++ finalBluetooth.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTserial(10, 11); // RX | TX
+
+int sensorPin = A0;
+int num = 0;
+int sensorValue = 0;
+
+void setup() {
+BTserial.begin(9600); 
+}
+
+void loop() {
+num++;
+//BTserial.print("UP_DOWN");
+//BTserial.print(",");
+//BTserial.print("UP_DOWN");
+//BTserial.print(",");
+//BTserial.print("UP_DOWN");
+//BTserial.print(",");
+//BTserial.print("UP_DOWN");
+//BTserial.print(",");
+//BTserial.print("UP,DOWN");
+//BTserial.print(";");
+//Serial.print(num);
+BTserial.print(num);
+
+delay(300);
+
+
+}
